f52a0afdd10a80b6d6046f598af98bf0b4ec2bfb
gen <kyliki@yandex.ru>
2025-09-17 02:00:02 +0300

    format lesson data

--- src/api/lessons/lesson/lesson.service.ts
+++ src/api/lessons/lesson/lesson.service.ts
@@ -16,14 +16,35 @@ export class LessonService {
     return this.dbLessonService.create(args);
   }
 
-  findAllBy(params: {
+  async findAllBy(params: {
     campId: number;
     startDate?: string;
     activityTypeId?: number;
     auditoriumId?: number;
     lessonTypeId?: number;
   }) {
-    return this.dbLessonService.findAllBy(params);
+    const data = await this.dbLessonService.findAllBy(params);
+
+    return data.map((lesson) => ({
+      id: lesson.id,
+      startDate: lesson.startDate,
+      endDate: lesson.endDate,
+      auditorium: lesson.auditorium,
+      lessonType: lesson.lessonType,
+      activityType: lesson.activityType,
+      groups: lesson.lesson_group.map((item) => ({
+        ...item.group,
+        lesson_group: item.id,
+      })),
+      sportsmen: lesson.lesson_sportsmen.map((item) => ({
+        ...item.sportsman,
+        lesson_sportsman: item.id,
+      })),
+      coaches: lesson.lesson_coaches.map((item) => ({
+        ...item.coach,
+        lesson_coach: item.id,
+      })),
+    }));
   }
 
   findOne(id: number) {
