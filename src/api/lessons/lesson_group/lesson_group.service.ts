eca29eac322256325fb9586ad1e9a91fc2740e2c
TimPulin <tigeronsun@gmail.com>
2025-09-18 19:50:50 +0300

    fix response of lesson_group, lesson_sportsman

--- src/api/lessons/lesson_group/lesson_group.service.ts
+++ src/api/lessons/lesson_group/lesson_group.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { DbLesson_GroupService } from '../../../db/db-module/schedule/lesson-group-participants/db-lesson_group.service';
+import { Lesson_Group } from '../../../db/db-module/schedule/lesson-group-participants/lesson_group.entity';
+
+function formatResponse(data: Lesson_Group) {
+  return {
+    id: data.id,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+    deleted_at: data.deleted_at,
+    lessonId: data.lesson.id,
+    groupId: data.group.id,
+  };
+}
 
 @Injectable()
 export class Lesson_GroupService {
   constructor(private dbLesson_GroupService: DbLesson_GroupService) {}
 
-  create(params: { lessonId: number; groupId: number }) {
-    return this.dbLesson_GroupService.create(params);
+  async create(params: { lessonId: number; groupId: number }) {
+    const data = await this.dbLesson_GroupService.create(params);
+    return formatResponse(data);
   }
 
-  update(id: number, params: { groupId: number }) {
-    return this.dbLesson_GroupService.update(id, params);
+  async update(id: number, params: { groupId: number }) {
+    const data = await this.dbLesson_GroupService.update(id, params);
+    return formatResponse(data);
   }
 
   remove(id: number) {
--- src/api/lessons/lesson_sportsman/lesson_sportsman.service.ts
+++ src/api/lessons/lesson_sportsman/lesson_sportsman.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { DbLesson_SportsmanParticipantsService } from '../../../db/db-module/schedule/lesson-sportsman-participants/db-lesson_sportsman-participants.service';
+import { Lesson_Sportsman } from '../../../db/db-module/schedule/lesson-sportsman-participants/lesson_sportsman.entity';
+
+function formatResponse(data: Lesson_Sportsman) {
+  return {
+    id: data.id,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+    deleted_at: data.deleted_at,
+    lessonId: data.lesson.id,
+    sportsmanId: data.sportsman.id,
+  };
+}
 
 @Injectable()
 export class Lesson_SportsmanService {
@@ -7,12 +19,14 @@ export class Lesson_SportsmanService {
     private dbLesson_SportsmanService: DbLesson_SportsmanParticipantsService,
   ) {}
 
-  create(params: { lessonId: number; sportsmanId: number }) {
-    return this.dbLesson_SportsmanService.create(params);
+  async create(params: { lessonId: number; sportsmanId: number }) {
+    const data = await this.dbLesson_SportsmanService.create(params);
+    return formatResponse(data);
   }
 
-  update(id: number, params: { sportsmanId: number }) {
-    return this.dbLesson_SportsmanService.update(id, params);
+  async update(id: number, params: { sportsmanId: number }) {
+    const data = await this.dbLesson_SportsmanService.update(id, params);
+    return formatResponse(data);
   }
 
   remove(id: number) {
