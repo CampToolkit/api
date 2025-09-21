6920ca4e446c177e9620bed88f86d4fa6006d8b0
TimPulin <tigeronsun@gmail.com>
2025-09-21 15:40:05 +0300

    update createLessonDto

--- src/api/lessons/lesson/dto/create-lesson.dto.ts
+++ src/api/lessons/lesson/dto/create-lesson.dto.ts
@@ -1,6 +1,17 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsDateString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsInt,
+  IsDateString,
+  IsArray,
+  ValidateNested,
+  ArrayNotEmpty,
+  IsOptional,
+} from 'class-validator';
 import { Type } from 'class-transformer';
+import {
+  CoachWithRoleDto,
+  LessonCoachRole,
+} from '../../lesson_coach/dto/create-lesson_coach.dto';
 
 export class CreateLessonDto {
   @ApiProperty({ example: '1', description: 'ID лагеря' })
@@ -36,4 +47,41 @@ export class CreateLessonDto {
   @Type(() => Number)
   @IsInt()
   lessonTypeId: number;
+
+  @ApiPropertyOptional({
+    type: [CoachWithRoleDto],
+    description: 'Список тренеров с ролями',
+    example: [
+      { coachId: 1, role: LessonCoachRole.PRIMARY },
+      { coachId: 2, role: LessonCoachRole.SECONDARY },
+    ],
+  })
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CoachWithRoleDto)
+  coaches: CoachWithRoleDto[];
+
+  @ApiPropertyOptional({
+    type: [Number],
+    example: [1, 2, 3],
+    description: 'массив ID групп, участвующих в Event',
+  })
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsInt({ each: true })
+  groupIds: number[];
+
+  @ApiPropertyOptional({
+    type: [Number],
+    example: [1, 2, 3],
+    description: 'массив ID спортсменов, участвующих в Event',
+  })
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsInt({ each: true })
+  sportsmanIds: number[];
 }
--- src/api/lessons/lesson_coach/dto/create-lesson_coach.dto.ts
+++ src/api/lessons/lesson_coach/dto/create-lesson_coach.dto.ts
@@ -7,17 +7,7 @@ export enum LessonCoachRole {
   SECONDARY = 'SECONDARY',
 }
 
-export class CreateLessonCoachDto {
-  @ApiProperty({
-    example: 1,
-    description: 'ID урока',
-    type: Number,
-  })
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  lessonId: number;
-
+export class CoachWithRoleDto {
   @ApiProperty({
     example: 5,
     description: 'ID тренера',
@@ -37,4 +27,16 @@ export class CreateLessonCoachDto {
     message: 'role must be either PRIMARY or SECONDARY',
   })
   role: LessonCoachRole;
+}
+
+export class CreateLessonCoachDto extends CoachWithRoleDto {
+  @ApiProperty({
+    example: 1,
+    description: 'ID урока',
+    type: Number,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  lessonId: number;
 }
